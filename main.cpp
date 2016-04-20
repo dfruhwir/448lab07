28b1670a8ae63cb5421a3c7aeb656f5e3a4b21ae
Dillon Fruhwirth <d487f080@ku.edu>
2016-04-20 18:57:55 -0500

    Add more tests on empty list

--- main.cpp
+++ main.cpp
@@ -15,5 +15,41 @@ int main()
 	{
 		std::cout << "FAIL!! :-(\n";
 	}
+	std::cout << "Test if size()==0: ";
+	if(p1.size()==0)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if removeFront()==false: ";
+	if(p1.removeFront()==false)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if removeBack()==false: ";
+	if(p1.removeBack()==false)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if search(7)==false: ";
+	if(p1.search(7)==false)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
 	return(0);
 }
