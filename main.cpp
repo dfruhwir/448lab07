0443a76482bfd45505c8f01e9eb626ef9bd6c1c9
Dillon Fruhwirth <d487f080@ku.edu>
2016-04-21 12:00:56 -0500

    Add more tests for list of size one

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 int main()
 {
 	LinkedList<int> p1;
+	LinkedList<int> p2;
+	LinkedList<int> p3;
 	int temp;
+	int temp2;
 	std::vector<int> v;
 	std::string list = "";
 
@@ -16,7 +19,6 @@ int main()
 	for(int x=0;x<v.size();x+=1)
 	{
 		list = list + std::to_string(v[x]) + ",";
-
 	}
 	if(list == "")
 	{
@@ -27,7 +29,7 @@ int main()
 		std::cout << "FAIL!! :-(\n";
 	}
 	std::cout << "Test if isEmpty()==true for initial list:                 ";
-	if(p1.isEmpty()==false)
+	if(p1.isEmpty()==true)
 	{
 		std::cout << "Pass!\n";
 	}
@@ -79,9 +81,102 @@ int main()
 	for(int x=0;x<v.size();x+=1)
 	{
 		list = list + std::to_string(v[x]) + ",";
-
 	}
 	if(list == "5,")
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if isEmpty()==false:                                 ";
+	if(p1.isEmpty()==false)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if size() increased by 1 for addFront:               ";
+	temp2 = p1.size();
+	if((temp2-temp)==1)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-( Has size increase by " << (temp2-temp) << "\n";
+	}
+	std::cout << "Test if removeFront()==true for list of size 1:           ";
+	if(p1.removeFront()==true)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if list is empty using toVector:                     ";
+	v=p1.toVector();
+	list = "";
+	for(int x=0;x<v.size();x+=1)
+	{
+		list = list + std::to_string(v[x]) + ",";
+	}
+	if(list == "")
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if addBack(5) works for empty list using toVector:   ";
+	//Switch to p2 in case problems have arisen
+	p2.addBack(5);
+	v=p2.toVector();
+	list = "";
+	for(int x=0;x<v.size();x+=1)
+	{
+		list = list + std::to_string(v[x]) + ",";
+	}
+	if(list == "5,")
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if size() increased by 1 for addBack:                ";
+	temp2 = p2.size();
+	if((temp2-temp)==1)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-( Has size increase by " << (temp2-temp) << "\n";
+	}
+	std::cout << "Test if removeBack()==true for list of size 1:            ";
+	if(p2.removeFront()==true)
+	{
+		std::cout << "Pass!\n";
+	}
+	else
+	{
+		std::cout << "FAIL!! :-(\n";
+	}
+	std::cout << "Test if list is empty using toVector:                     ";
+	v=p2.toVector();
+	list = "";
+	for(int x=0;x<v.size();x+=1)
+	{
+		list = list + std::to_string(v[x]) + ",";
+	}
+	if(list == "")
 	{
 		std::cout << "Pass!\n";
 	}
